b0a1d04e1096458b2da695aa6c9c652d63134e88
Luciano <44642280+lcismondi@users.noreply.github.com>
2024-05-23 14:40:46 -0300

    Crea script para unificar nav y footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
+function loadHTML(selector, url) {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to load ${url}`);
+        }
+        return response.text();
+      })
+      .then(data => {
+        document.querySelector(selector).innerHTML = data;
+      })
+      .catch(error => console.error(error));
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    loadHTML('nav', 'nav.html');
+    loadHTML('footer', 'footer.html');
+  });
 
